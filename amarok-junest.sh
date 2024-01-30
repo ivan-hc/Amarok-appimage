03b681d0a044ddff15cce2851f27180d01b5d93d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-01-30 02:56:07 +0100

    Update amarok-junest.sh

--- amarok-junest.sh
+++ amarok-junest.sh
@@ -3,7 +3,7 @@
 # NAME OF THE APP BY REPLACING "SAMPLE"
 APP=amarok
 BIN="amarok" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="ffmpeg fftw gmock kcmutils5 kdnssd5 kirigami2 knewstuff5 ktexteditor5 libgpod liblastfm-qt5 libmariadbclient libmtp libmygpo-qt5 loudmouth phonon-qt5 qt5-declarative qt5-webengine taglib-extras threadweaver5 pulseaudio"
+DEPENDENCES="ffmpeg fftw gmock kcmutils5 kdnssd5 kirigami2 knewstuff5 ktexteditor5 libgpod liblastfm-qt5 libmariadbclient libmtp libmygpo-qt5 loudmouth phonon-qt5 qt5-declarative qt5-webengine taglib taglib-extras threadweaver5 pulseaudio"
 BASICSTUFF="binutils gzip"
 COMPILERS="base-devel"
 
