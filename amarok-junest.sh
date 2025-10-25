7e1e27d07832b3e4adad6552650d1802f94d8c9f
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-10-25 02:13:26 +0200

    Add libmpg

--- amarok-junest.sh
+++ amarok-junest.sh
@@ -16,7 +16,7 @@ lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
 LIBSAVED="libsodium.so libsoxr.so libproxy libduktape.so libFLAC.so libogg libmp3lame libvorbis libnghttp libssh libpsl.so libxcb qt libuuid \
 libGLX libglapi.so libXxf libSPIRV-Tools.so libsensors.so libxshmfence.so libdrm libpciaccess.so \
-svg mysql libnss_mdns gdk-pixbuf json libnss_mdns $lib_audio_keywords $lib_browser_launcher"
+svg mysql libnss_mdns gdk-pixbuf json libnss_mdns libmpg $lib_audio_keywords $lib_browser_launcher"
 
 [ -n "$lib_browser_launcher" ] && DEPENDENCES="$DEPENDENCES xapp hicolor-icon-theme"
 
